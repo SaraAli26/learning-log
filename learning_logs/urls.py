d17817876b13131df4e4b8a3d6fff9c38810d255
Sara Ahmed <saraahmed2693@gmail.com>
2018-06-26 00:51:16 +0200

    Django Forms - issue

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,4 +11,9 @@ urlpatterns = [
     path('base/', views.base, name="base"),
     #Dispaly all topics
     path('topics/', views.topics, name="topics"),
+    #Display the seperate entries for each topic
+    path('topics/<int:topic_id>/', views.topic, name="topic"),
+    # Add the urls for the forms for users other than admin to insert data_added
+    path('new_topic/', views.new_topic, name='new_topic'),
+
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-from learning_logs.models import Topic
+from learning_logs.models import Topic, Entry
+from .forms import TopicForm
+
 # Create your views here.
 def index(request):
     """The home page for Learning Log"""
@@ -15,3 +19,25 @@ def topics(request):
     topics = Topic.objects.all()
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Dispaly the Entries for each topic"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.all()
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
+#method for forms for data to be entered by user
+def new_topic(request):
+    """Add a new topic by users other than admin."""
+    if request.method != 'POST':
+        # Condition check if there is data submitted or not, if not then create an empty form
+        form = TopicForm()
+    else:
+        # If request is post then save data to DB
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs/topics.html'))
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
