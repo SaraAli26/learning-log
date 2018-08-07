4cb108d068c34644ad11ddb323b37add38972665
Sara Ahmed <saraahmed2693@gmail.com>
2018-08-07 15:40:53 +0200

    Run on server sucesfully

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -147,8 +147,8 @@ if os.getcwd() == '/app':
     # Allow all host haders.
     ALLOWED_HOSTS = ['*']
 
-    #Static asset configuration
-    BASE_DIR = os.path.dirname(os.path.abspath(_file_))
+    #Static asset configuration                 
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
     STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
